3f140fd49e440cb87f98eaf8fba0a302fb82428b
Ivan Vecera <ivan@cera.cz>
2021-06-17 22:56:57 +0200

    sdm710-common: remove 32bit liblbs_core.so library
    The library is not used anymore to link libraries like
    libbatching.so, libgeofencing... So we can now safely remove
    its 32bit variant and keep only 64bit one (dependency for GNSS
    service).
    
    Change-Id: I68bdd76ca9b08f68cc2b9cfc5f05bf63993f0777
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- sdm710-common/sdm710-common-vendor.mk
+++ sdm710-common/sdm710-common-vendor.mk
@@ -577,6 +577,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/libizat_client_api.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libizat_client_api.so \
     vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/libizat_core.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libizat_core.so \
     vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/libjnihelper.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libjnihelper.so \
+    vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/liblbs_core.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblbs_core.so \
     vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/liblearningmodule.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblearningmodule.so \
     vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/liblistensoundmodel2.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblistensoundmodel2.so \
     vendor/xiaomi/sdm710-common/proprietary/vendor/lib64/libllvm-glnext.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libllvm-glnext.so \
@@ -768,7 +769,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm710-common/proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
-    liblbs_core \
     SoterService \
     CneApp \
     IWlanService \
